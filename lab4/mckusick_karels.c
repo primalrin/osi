08c0fc665cbe3fe9ff104733f297579ceae33e8f
primalrin <shestkon@gmail.com>
2025-01-08 18:38:36 +0300

    last fix

--- lab4/mckusick_karels.c
+++ lab4/mckusick_karels.c
@@ -1,161 +1,97 @@
 #include "allocator.h"
 #include <stddef.h>
-#include <windows.h>
+#include <math.h>
+#include <stdint.h>
 
-#define MIN_ALLOCATION_SIZE 16
-#define ALIGNMENT 8
-
-typedef struct FreeBlock
-{
-    size_t size;
-    struct FreeBlock *next;
-} FreeBlock;
+#define BLOCK_SIZE 16
 
 struct Allocator
 {
     void *memory;
     size_t size;
-    FreeBlock *free_list;
+    size_t max_order;
+    uint8_t *free_blocks;
 };
 
-static void *sys_alloc(size_t size)
+size_t get_order(size_t size)
 {
-    return VirtualAlloc(NULL, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    return (size_t)ceil(log2(size));
 }
 
-static void sys_free(void *ptr, size_t size)
+Allocator *allocator_create(void *memory, size_t size)
 {
-    VirtualFree(ptr, 0, MEM_RELEASE);
-}
-
-static size_t align(size_t size)
-{
-    return (size + ALIGNMENT - 1) & ~(ALIGNMENT - 1);
-}
-
-Allocator *allocator_create(void *const memory, const size_t size)
-{
-    if (memory == NULL || size == 0)
+    Allocator *allocator = (Allocator *)memory;
+    allocator->memory = (uint8_t *)memory + sizeof(Allocator);
+    allocator->size = size - sizeof(Allocator);
+    allocator->max_order = (size_t)log2(allocator->size);
+    size_t bitmap_size = (size / BLOCK_SIZE) / 8;
+    if ((size / BLOCK_SIZE) % 8 != 0)
     {
-        return NULL;
+        bitmap_size++;
     }
 
-    Allocator *allocator = (Allocator *)memory;
-    allocator->memory = (char *)memory + sizeof(Allocator);
-    allocator->size = size - sizeof(Allocator);
-    allocator->free_list = (FreeBlock *)allocator->memory;
-    allocator->free_list->size = allocator->size;
-    allocator->free_list->next = NULL;
+    allocator->free_blocks = (uint8_t *)allocator->memory + allocator->size - bitmap_size;
+    for (size_t i = 0; i < bitmap_size; i++)
+    {
+        allocator->free_blocks[i] = 0xFF;
+    }
 
     return allocator;
 }
 
-void allocator_destroy(Allocator *const allocator)
+void allocator_destroy(Allocator *allocator)
 {
 }
 
-void *allocator_alloc(Allocator *const allocator, const size_t size)
+void *allocator_alloc(Allocator *allocator, size_t size)
 {
-    if (allocator == NULL || size == 0)
+    size_t num_blocks = (size + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+    size_t total_blocks = allocator->size / BLOCK_SIZE;
+
+    for (size_t i = 0; i <= total_blocks - num_blocks; i++)
     {
-        return NULL;
-    }
-
-    size_t alloc_size = align(size + sizeof(size_t));
-
-    FreeBlock *current = allocator->free_list;
-    FreeBlock *prev = NULL;
-
-    while (current != NULL)
-    {
-        if (current->size >= alloc_size)
+        int found = 1;
+        for (size_t j = 0; j < num_blocks; j++)
         {
-
-            if (current->size >= alloc_size + sizeof(FreeBlock) + MIN_ALLOCATION_SIZE)
+            size_t byte_index = (i + j) / 8;
+            size_t bit_index = (i + j) % 8;
+            if (!(allocator->free_blocks[byte_index] & (1 << bit_index)))
             {
-                FreeBlock *new_block = (FreeBlock *)((char *)current + alloc_size);
-                new_block->size = current->size - alloc_size;
-                new_block->next = current->next;
-
-                if (prev == NULL)
-                {
-                    allocator->free_list = new_block;
-                }
-                else
-                {
-                    prev->next = new_block;
-                }
-                current->size = alloc_size;
+                found = 0;
+                break;
             }
-            else
-            {
-                if (prev == NULL)
-                {
-                    allocator->free_list = current->next;
-                }
-                else
-                {
-                    prev->next = current->next;
-                }
-            }
-
-            size_t *block_size = (size_t *)current;
-            *block_size = current->size - sizeof(size_t);
-            return (char *)current + sizeof(size_t);
         }
 
-        prev = current;
-        current = current->next;
+        if (found)
+        {
+            for (size_t j = 0; j < num_blocks; j++)
+            {
+                size_t byte_index = (i + j) / 8;
+                size_t bit_index = (i + j) % 8;
+                allocator->free_blocks[byte_index] &= ~(1 << bit_index);
+            }
+            return (uint8_t *)allocator->memory + i * BLOCK_SIZE;
+        }
     }
 
     return NULL;
 }
 
-void allocator_free(Allocator *const allocator, void *const memory)
+void allocator_free(Allocator *allocator, void *memory)
 {
-    if (allocator == NULL || memory == NULL)
+    if (memory == NULL)
     {
         return;
     }
+    size_t offset = (uint8_t *)memory - (uint8_t *)allocator->memory;
+    size_t index = offset / BLOCK_SIZE;
 
-    size_t *block_size = (size_t *)((char *)memory - sizeof(size_t));
-    FreeBlock *freed_block = (FreeBlock *)((char *)memory - sizeof(size_t));
-    freed_block->size = *block_size + sizeof(size_t);
-
-    FreeBlock *current = allocator->free_list;
-    FreeBlock *prev = NULL;
-
-    while (current != NULL && current < freed_block)
+    if (index < (allocator->size / BLOCK_SIZE))
     {
-        prev = current;
-        current = current->next;
-    }
+        size_t byte_index = index / 8;
+        size_t bit_index = index % 8;
 
-    if (prev == NULL)
-    {
-        freed_block->next = allocator->free_list;
-        allocator->free_list = freed_block;
-    }
-    else
-    {
-        freed_block->next = prev->next;
-        prev->next = freed_block;
-    }
-
-    current = allocator->free_list;
-    prev = NULL;
-    while (current != NULL)
-    {
-        if (prev != NULL && (char *)prev + prev->size == (char *)current)
-        {
-            prev->size += current->size;
-            prev->next = current->next;
-            current = prev->next;
-        }
-        else
-        {
-            prev = current;
-            current = current->next;
-        }
+        allocator->free_blocks[byte_index] |= (1 << bit_index);
     }
 }
